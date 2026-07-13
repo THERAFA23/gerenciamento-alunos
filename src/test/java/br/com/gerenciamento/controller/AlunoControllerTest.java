e86e4b21ff6794a666a11a8fe0a1db219a18e8be
Rafael <carvalhorafael527@gmail.com>
2026-07-12 21:28:26 -0300

    Testes integração

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,113 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.repository.AlunoRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Before
+    public void setup() {
+        alunoRepository.deleteAll();
+    }
+
+    @Test
+    public void testPesquisarAlunoNomeVazioRetornaListaCompleta() throws Exception {
+        
+        Aluno aluno1 = new Aluno();
+
+        aluno1.setNome("Mauricio Torres");
+        aluno1.setMatricula("111222");
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.ATIVO);
+        aluno1.setTurno(Turno.MATUTINO);
+        alunoRepository.save(aluno1);
+
+        Aluno aluno2 = new Aluno();
+
+        aluno2.setNome("Beatriz Cavalcanti");
+        aluno2.setMatricula("333444");
+        aluno2.setCurso(Curso.INFORMATICA);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setTurno(Turno.NOTURNO);
+        alunoRepository.save(aluno2);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/pesquisar-aluno")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("nome", ""))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("Aluno/pesquisa-resultado"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("ListaDeAlunos"))
+                // Garante que ambos os alunos cadastrados foram retornados na lista completa
+                .andExpect(MockMvcResultMatchers.model().attribute("ListaDeAlunos", hasSize(2)));
+    }
+
+    @Test
+    public void testInserirAlunoComSucesso() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/InsertAlunos")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("nome", "Adriano Lima")
+                .param("matricula", "123457")
+                .param("curso", Curso.INFORMATICA.name())
+                .param("status", Status.ATIVO.name())
+                .param("turno", Turno.MATUTINO.name()))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/alunos-adicionados"));
+    }
+
+    
+    @Test
+    public void testInserirAlunoComErroValidacao() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/InsertAlunos")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("nome", "Igo") 
+                .param("matricula", "123")
+                .param("curso", Curso.ADMINISTRACAO.name())
+                .param("status", Status.ATIVO.name())
+                .param("turno", Turno.NOTURNO.name()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("Aluno/formAluno"))
+                .andExpect(MockMvcResultMatchers.model().hasErrors());
+    }
+
+    @Test
+    public void testRemoverAlunoExistente() throws Exception {
+        Aluno aluno = new Aluno();
+
+        aluno.setNome("Aluno Exclusao");
+        aluno.setMatricula("99999");
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+        alunoRepository.save(aluno);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/remover/" + aluno.getId()))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/alunos-adicionados"));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,85 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.repository.UsuarioRepository;
+import br.com.gerenciamento.util.Util;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.not;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Before
+    public void setup() {
+        usuarioRepository.deleteAll();
+    }
+
+    
+    @Test
+    public void testLoginInvalidoNaoRedirecionaParaIndex() throws Exception {
+        
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("user", "usuarioInexistente")
+                .param("senha", "senhaErrada"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+
+                .andExpect(MockMvcResultMatchers.view().name("login/cadastro"))
+                .andExpect(MockMvcResultMatchers.view().name(not("home/index")))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("msg"));
+    }
+
+    @Test
+    public void testLoginComSucessoRedirecionaParaIndex() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setEmail("valido@teste.com");
+        usuario.setUser("uservalido");
+        usuario.setSenha(Util.md5("senha123")); // O banco armazena a senha hash MD5
+        usuarioRepository.save(usuario);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("user", "uservalido")
+                .param("senha", "senha123"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home/index"))
+                .andExpect(MockMvcResultMatchers.request().sessionAttribute("usuarioLogado", org.hamcrest.Matchers.notNullValue()));
+    }
+    
+    @Test
+    public void testCadastrarUsuarioComSucesso() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/salvarUsuario")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("email", "novousuario@teste.com")
+                .param("user", "novouser")
+                .param("senha", "minhasenha"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
+    }
+
+    @Test
+    public void testLogoutInvalidaSessao() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/logout"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("login/login"));
+    }
 }
